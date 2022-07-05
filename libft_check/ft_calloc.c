0951233b8b64f837a34cba7e5b85d87898cbc601
Jamie135 <98770913+Jamie135@users.noreply.github.com>
2022-07-05 16:43:59 +0200

    Add files via upload

---
+++ libft_check/ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbureera <pbureera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 14:56:09 by pbureera          #+#    #+#             */
+/*   Updated: 2022/07/04 12:24:31 by pbureera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (nmemb && (nmemb * size / nmemb) != size)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
